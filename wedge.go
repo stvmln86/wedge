5d3118e6ba10c49ff4d463ffeac8cf94fb42956c
Stephen Malone <mail@wirehaiku.org>
2024-12-08 07:16:54 +1000

    Changed code to panic instead of return errors.

--- wedge.go
+++ wedge.go
@@ -53,16 +53,15 @@ func Write(a any) {
 // Stack is a last-in-first-out stack of stored integers.
 var Stack []int
 
-// Pop removes and returns the top N integers on the Stack.
-func Pop(n int) ([]int, error) {
-	if len(Stack) < n {
-		return nil, fmt.Errorf("Stack is insufficient")
+// Pop removes and returns the top integer on the Stack.
+func Pop() int {
+	if len(Stack) == 0 {
+		panic("stack is insufficient")
 	}
 
-	is := Stack[len(Stack)-n:]
-	Stack = Stack[:len(Stack)-n]
-	slices.Reverse(is)
-	return is, nil
+	i := Stack[len(Stack)-1]
+	Stack = Stack[:len(Stack)-1]
+	return i
 }
 
 // Push appends one or more integers to the top of the Stack.
@@ -78,26 +77,26 @@ func Push(is ...int) {
 var Queue []any
 
 // Dequeue removes and returns the next atom in the Queue.
-func Dequeue() (any, error) {
+func Dequeue() any {
 	if len(Queue) == 0 {
-		return nil, fmt.Errorf("Queue is empty")
+		panic("queue is empty")
 	}
 
 	a := Queue[0]
 	Queue = Queue[1:]
-	return a, nil
+	return a
 }
 
 // DequeueTo removes and all atoms up to an atom in the Queue.
-func DequeueTo(a any) ([]any, error) {
+func DequeueTo(a any) []any {
 	i := slices.Index(Queue, a)
 	if i < 0 {
-		return nil, fmt.Errorf("Queue is missing Atom %q", a)
+		panic("queue is insufficient")
 	}
 
 	as := Queue[:i]
 	Queue = Queue[i+1:]
-	return as, nil
+	return as
 }
 
 // Enqueue appends an atom slice to the end of the Queue.
@@ -130,76 +129,49 @@ func Parse(s string) []any {
 ///////////////////////////////////////////////////////////////////////////////////////
 
 // Oper is a global operator function.
-type Oper func() error
+type Oper func()
 
 // Opers is a global map of all existing Oper functions.
 var Opers = make(map[string]Oper)
-
-// Wrap wraps an integer slice function with a Stack error.
-func Wrap(n int, ifun func([]int)) Oper {
-	return func() error {
-		is, err := Pop(n)
-		if err != nil {
-			return err
-		}
-
-		ifun(is)
-		return nil
-	}
-}
 
 ///////////////////////////////////////////////////////////////////////////////////////
 //                                part 6 · the runtime                               //
 ///////////////////////////////////////////////////////////////////////////////////////
 
 // Evaluate evaluates the next atom in the Queue.
-func Evaluate(a any) error {
+func Evaluate(a any) {
 	switch a := a.(type) {
 	case int:
 		Push(a)
-		return nil
 	case string:
 		f, b := Opers[a]
 		if !b {
-			return fmt.Errorf("invalid name %q", a)
+			panic("invalid reference")
 		}
 
-		return f()
+		f()
 	default:
-		return fmt.Errorf("invalid atom %q", a)
+		panic("invalid atom type")
 	}
 }
 
 // EvaluateQueue evaluates all atoms in the Queue.
-func EvaluateQueue() error {
+func EvaluateQueue() {
 	for len(Queue) > 0 {
-		a, err := Dequeue()
-		if err != nil {
-			return err
-		}
-
-		if err := Evaluate(a); err != nil {
-			return err
-		}
+		Evaluate(Dequeue())
 	}
-
-	return nil
 }
 
 // EvaluateSlice evaluates all atoms in an atom slice.
-func EvaluateSlice(as []any) error {
+func EvaluateSlice(as []any) {
 	for _, a := range as {
-		if err := Evaluate(a); err != nil {
-			return err
-		}
+		Evaluate(a)
 	}
-
-	return nil
 }
 
 // EvaluateString evaluates all atoms in a parsed string.
-func EvaluateString(s string) error {
-	return EvaluateSlice(Parse(s))
+func EvaluateString(s string) {
+	EvaluateSlice(Parse(s))
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -210,15 +182,19 @@ func EvaluateString(s string) error {
 func RunREPL() {
 	r := bufio.NewReader(Stdin)
 
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Fprintf(Stdout, "Error: %s.\n", r)
+		}
+	}()
+
 	for {
 		fmt.Fprintf(Stdout, "> ")
 		s, _ := r.ReadString('\n')
 		Enqueue(Parse(s))
 
-		if err := EvaluateQueue(); err != nil {
-			fmt.Fprintf(Stdout, "Error: %s.\n", err.Error())
-
-		} else if len(Stack) > 0 {
+		EvaluateQueue()
+		if len(Stack) > 0 {
 			fmt.Fprintf(Stdout, ": %v\n", Stack)
 		}
 	}
@@ -230,38 +206,28 @@ func RunREPL() {
 
 func init() {
 	// Mathematical functions.
-	Opers["+"] = Wrap(2, func(is []int) { Push(is[0] + is[1]) })
-	Opers["-"] = Wrap(2, func(is []int) { Push(is[0] - is[1]) })
-	Opers["*"] = Wrap(2, func(is []int) { Push(is[0] * is[1]) })
-	Opers["/"] = Wrap(2, func(is []int) { Push(is[0] / is[1]) })
-	Opers["%"] = Wrap(2, func(is []int) { Push(is[0] % is[1]) })
+	Opers["+"] = func() { Push(Pop() + Pop()) }
+	Opers["-"] = func() { Push(Pop() - Pop()) }
+	Opers["*"] = func() { Push(Pop() * Pop()) }
+	Opers["/"] = func() { Push(Pop() / Pop()) }
+	Opers["%"] = func() { Push(Pop() % Pop()) }
 
 	// Stack functions.
-	Opers["&"] = Wrap(1, func(is []int) { Push(is[0], is[0]) })
-	Opers["~"] = Wrap(2, func(is []int) { Push(is[0], is[1]) })
-	Opers["@"] = Wrap(3, func(is []int) { Push(is[1], is[0], is[2]) })
+	Opers["&"] = func() { Push(Stack[len(Stack)-1]) }
+	// Opers["~"] = func() { Push(is[0], is[1]) }
+	// Opers["@"] = func() { Push(is[1], is[0], is[2]) }
+	Opers["#"] = func() { Push(len(Stack)) }
 
 	// Input/output functions.
-	Opers["."] = Wrap(1, func(is []int) { Write(is[0]) })
-	Opers[","] = Wrap(0, func(is []int) { Push(Read()) })
+	Opers["."] = func() { Write(Pop()) }
+	Opers[","] = func() { Push(Read()) }
 
 	// Logic functions.
-	Opers["?["] = func() error {
-		is, err := Pop(1)
-		if err != nil {
-			return err
+	Opers["?["] = func() {
+		as := DequeueTo("]?")
+		if Pop() != 0 {
+			EvaluateSlice(as)
 		}
-
-		as, err := DequeueTo("]?")
-		if err != nil {
-			return err
-		}
-
-		if is[0] != 0 {
-			return EvaluateSlice(as)
-		}
-
-		return nil
 	}
 }
 
