a510d3e5f9c95c6d2884dce95ad0205c62295089
Stephen Malone <mail@wirehaiku.org>
2024-12-08 14:03:34 +1000

    Condensed project back into single file.

--- wedge.go
+++ wedge.go
@@ -1,38 +1,256 @@
+///////////////////////////////////////////////////////////////////////////////////////
+//             wedge · a minimal stack language in Go · by Stephen Malone            //
+///////////////////////////////////////////////////////////////////////////////////////
+
 package main
 
 import (
 	"bufio"
 	"fmt"
-
-	"github.com/stvmln86/wedge/wedge"
+	"io"
+	"os"
+	"os/signal"
+	"slices"
+	"strconv"
+	"strings"
 )
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                            part 1 · globals & constants                           //
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Opers is the global operator function map.
+var Opers = make(map[string]func())
+
+// Running is the global program running indicator.
+var Running = true
+
+// Stdin is the global input stream.
+var Stdin io.Reader = os.Stdin
+
+// Stdout is the global output stream.
+var Stdout io.Writer = os.Stdout
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                              part 2 · input & output                              //
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Read returns an input byte from Stdin as an integer.
+func Read() int {
+	var bs = make([]byte, 1)
+	Stdin.Read(bs)
+	return int(bs[0])
+}
+
+// Write writes a byte or integer to Stdout.
+func Write(a any) {
+	switch a := a.(type) {
+	case byte:
+		Stdout.Write([]byte{a})
+	case int:
+		s := fmt.Sprintf("%c", a)
+		Stdout.Write([]byte(s))
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                           part 3 · the stack & functions                          //
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Stack is a last-in-first-out stack of stored integers.
+var Stack []int
+
+// Peek returns the top integer on the Stack.
+func Peek() int {
+	if len(Stack) == 0 {
+		panic("stack is insufficient")
+	}
+
+	return Stack[len(Stack)-1]
+}
+
+// Pop removes and returns the top integer on the Stack.
+func Pop() int {
+	if len(Stack) == 0 {
+		panic("stack is insufficient")
+	}
+
+	i := Stack[len(Stack)-1]
+	Stack = Stack[:len(Stack)-1]
+	return i
+}
+
+// Push appends one or more integers to the top of the Stack.
+func Push(is ...int) {
+	Stack = append(Stack, is...)
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                           part 4 · the queue & functions                          //
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Queue is a first-in-first-out queue of parsed atoms.
+var Queue []any
+
+// Dequeue removes and returns the next atom in the Queue.
+func Dequeue() any {
+	if len(Queue) == 0 {
+		panic("queue is empty")
+	}
+
+	a := Queue[0]
+	Queue = Queue[1:]
+	return a
+}
+
+// DequeueTo removes and all atoms up to an atom in the Queue.
+func DequeueTo(a any) []any {
+	i := slices.Index(Queue, a)
+	if i < 0 {
+		panic("queue is insufficient")
+	}
+
+	as := Queue[:i]
+	Queue = Queue[i+1:]
+	return as
+}
+
+// Enqueue appends an atom sequence to the end of the Queue.
+func Enqueue(as ...any) {
+	Queue = append(Queue, as...)
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                           part 5 · parsing & evaluating                           //
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Parse returns a parsed atom slice from a string.
+func Parse(s string) []any {
+	var as []any
+
+	for _, s := range strings.Fields(strings.ToLower(s)) {
+		if i, err := strconv.Atoi(s); err == nil {
+			as = append(as, i)
+		} else {
+			as = append(as, s)
+		}
+	}
+
+	return as
+}
+
+// Evaluate evaluates the next atom in the Queue.
+func Evaluate(a any) {
+	switch a := a.(type) {
+	case int:
+		Push(a)
+	case string:
+		f, ok := Opers[a]
+		if !ok {
+			panic("invalid reference")
+		}
+
+		f()
+	default:
+		panic("invalid atom type")
+	}
+}
+
+// EvaluateQueue evaluates all atoms in the Queue.
+func EvaluateQueue() {
+	for len(Queue) > 0 {
+		Evaluate(Dequeue())
+	}
+}
+
+// EvaluateSlice evaluates all atoms in an atom slice.
+func EvaluateSlice(as []any) {
+	for _, a := range as {
+		Evaluate(a)
+	}
+}
+
+// EvaluateString evaluates all atoms in a parsed string.
+func EvaluateString(s string) {
+	EvaluateSlice(Parse(s))
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                            part 6 · operator functions                            //
+///////////////////////////////////////////////////////////////////////////////////////
+
+// InitOpers initialises the Opers map with default operator functions.
+func InitOpers() {
+	// Mathematical functions.
+	Opers["+"] = func() { Push(Pop() + Pop()) }
+	Opers["-"] = func() { Push(Pop() - Pop()) }
+	Opers["*"] = func() { Push(Pop() * Pop()) }
+	Opers["/"] = func() { Push(Pop() / Pop()) }
+	Opers["%"] = func() { Push(Pop() % Pop()) }
+
+	// Stack functions.
+	Opers["&"] = func() { Push(Peek()) }
+	Opers["#"] = func() { Push(len(Stack)) }
+	Opers["~"] = func() { Push(Pop(), Pop()) }
+	Opers["@"] = func() {
+		a, b, c := Pop(), Pop(), Pop()
+		Push(b, a, c)
+	}
+
+	// Input/output functions.
+	Opers["."] = func() { Write(Pop()) }
+	Opers[","] = func() { Push(Read()) }
+
+	// System functions.
+	Opers["dump"] = func() { fmt.Fprintf(Stdout, ": %v\n", Stack) }
+	Opers["exit"] = func() { Running = false }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                            part 7 · top-level commands                            //
+///////////////////////////////////////////////////////////////////////////////////////
 
 // RunREPL launches a read-eval-print loop.
 func RunREPL() {
-	r := bufio.NewReader(wedge.Stdin)
+	r := bufio.NewReader(Stdin)
 
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Fprintf(wedge.Stdout, "Error: %s.\n", r)
-		}
-	}()
-
-	for {
-		fmt.Fprintf(wedge.Stdout, "> ")
+	for Running {
+		fmt.Fprintf(Stdout, "> ")
 		s, _ := r.ReadString('\n')
+		as := Parse(s)
 
-		wedge.Enqueue(wedge.Parse(s)...)
-		wedge.EvaluateQueue()
+		if len(as) != 0 {
+			Enqueue(as...)
+			EvaluateQueue()
 
-		if len(wedge.Stack) > 0 {
-			fmt.Fprintf(wedge.Stdout, ": %v\n", wedge.Stack)
+			if len(Stack) > 0 {
+				Opers["dump"]()
+			}
 		}
 	}
 }
 
+///////////////////////////////////////////////////////////////////////////////////////
+//                               part 8 · main function                              //
+///////////////////////////////////////////////////////////////////////////////////////
+
+func init() {
+	InitOpers()
+}
+
 func main() {
-	switch {
-	default:
-		RunREPL()
-	}
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Fprintf(Stdout, "Error: %s.\n", r)
+		}
+	}()
+
+	go func() {
+		s := make(chan os.Signal, 1)
+		signal.Notify(s, os.Interrupt)
+		<-s
+		os.Exit(0)
+	}()
+
+	RunREPL()
 }
---
+++ wedge_test.go
@@ -0,0 +1,224 @@
+///////////////////////////////////////////////////////////////////////////////////////
+//                            wedge · unit tests & helpers                           //
+///////////////////////////////////////////////////////////////////////////////////////
+
+package main
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                           part 0 · unit testing helpers                           //
+///////////////////////////////////////////////////////////////////////////////////////
+
+func assertOper(t *testing.T, s string, is ...int) {
+	mockData(s)
+	EvaluateQueue()
+	if len(is) == 0 {
+		assert.Empty(t, Stack)
+	} else {
+		assert.Equal(t, is, Stack)
+	}
+}
+
+func mockData(s string, is ...int) {
+	Queue = Parse(s)
+	Stack = is
+}
+
+func mockStream(s string) *bytes.Buffer {
+	Stdin = bytes.NewBufferString(s)
+	Stdout = bytes.NewBuffer(nil)
+	return Stdout.(*bytes.Buffer)
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                            part 1 · globals & constants                           //
+///////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                              part 2 · input & output                              //
+///////////////////////////////////////////////////////////////////////////////////////
+
+func TestRead(t *testing.T) {
+	// setup
+	mockStream("test\n")
+
+	// success
+	i := Read()
+	assert.Equal(t, int('t'), i)
+}
+
+func TestWrite(t *testing.T) {
+	// setup
+	b := mockStream("")
+
+	// success
+	Write(byte('t'))
+	Write(int('t'))
+	assert.Equal(t, "tt", b.String())
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                           part 3 · the stack & functions                          //
+///////////////////////////////////////////////////////////////////////////////////////
+
+func TestPeek(t *testing.T) {
+	// setup
+	mockData("", 1)
+
+	// success
+	i := Peek()
+	assert.Equal(t, 1, i)
+}
+
+func TestPop(t *testing.T) {
+	// setup
+	mockData("", 1)
+
+	// success
+	i := Pop()
+	assert.Equal(t, 1, i)
+	assert.Empty(t, Stack)
+}
+
+func TestPush(t *testing.T) {
+	// setup
+	mockData("")
+
+	// success
+	Push(1)
+	assert.Equal(t, []int{1}, Stack)
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                           part 4 · the queue & functions                          //
+///////////////////////////////////////////////////////////////////////////////////////
+
+func TestDequeue(t *testing.T) {
+	// setup
+	mockData("a")
+
+	// success
+	a := Dequeue()
+	assert.Equal(t, "a", a)
+	assert.Empty(t, Queue)
+}
+
+func TestDequeueTo(t *testing.T) {
+	// setup
+	mockData("a end")
+
+	// success
+	as := DequeueTo("end")
+	assert.Equal(t, []any{"a"}, as)
+	assert.Empty(t, Queue)
+}
+
+func TestEnqueue(t *testing.T) {
+	// setup
+	mockData("")
+
+	// success
+	Enqueue("a")
+	assert.Equal(t, []any{"a"}, Queue)
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                           part 5 · parsing & evaluating                           //
+///////////////////////////////////////////////////////////////////////////////////////
+
+func TestParse(t *testing.T) {
+	// success
+	as := Parse("\t1 A\n")
+	assert.Equal(t, []any{1, "a"}, as)
+}
+
+func TestEvaluate(t *testing.T) {
+	// setup
+	InitOpers()
+	mockData("")
+
+	// success - int
+	Evaluate(1)
+	assert.Equal(t, []int{1}, Stack)
+
+	// success - string
+	Evaluate("&")
+	assert.Equal(t, []int{1, 1}, Stack)
+}
+
+func TestEvaluateQueue(t *testing.T) {
+	// setup
+	mockData("1 &")
+
+	// success
+	EvaluateQueue()
+	assert.Equal(t, []int{1, 1}, Stack)
+}
+
+func TestEvaluateSlice(t *testing.T) {
+	// setup
+	mockData("")
+
+	// success
+	EvaluateSlice([]any{1, "&"})
+	assert.Equal(t, []int{1, 1}, Stack)
+}
+
+func TestEvaluateString(t *testing.T) {
+	// setup
+	mockData("")
+
+	// success
+	EvaluateString("1 &")
+	assert.Equal(t, []int{1, 1}, Stack)
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                            part 6 · operator functions                            //
+///////////////////////////////////////////////////////////////////////////////////////
+
+func TestInitOpers(t *testing.T) {
+	// setup
+	InitOpers()
+	b := mockStream("test\n")
+
+	// success - mathematical functions
+	assertOper(t, "1 2 +", 3)
+	assertOper(t, "1 2 -", 1)
+	assertOper(t, "2 3 *", 6)
+	assertOper(t, "3 6 /", 2)
+	assertOper(t, "2 5 %", 1)
+
+	// success - stack functions
+	assertOper(t, "1 &", 1, 1)
+	assertOper(t, "1 #", 1, 1)
+	assertOper(t, "1 2 ~", 2, 1)
+	assertOper(t, "1 2 3 @", 2, 3, 1)
+
+	// success - input/output functions
+	assertOper(t, "116 .")
+	assertOper(t, ",", int('t'))
+	assert.Equal(t, "t", b.String())
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//                            part 7 · top-level commands                            //
+///////////////////////////////////////////////////////////////////////////////////////
+
+func TestRunREPL(t *testing.T) {
+	// setup
+	InitOpers()
+	mockData("")
+	b := mockStream("1 & exit\n")
+
+	// success
+	RunREPL()
+	assert.Equal(t, []int{1, 1}, Stack)
+	assert.Equal(t, "> : [1 1]\n", b.String())
+}
